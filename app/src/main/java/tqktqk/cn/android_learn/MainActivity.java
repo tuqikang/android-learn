2929b5d5fa1f7fbc51e35bea6527ca93d466af43
tqk <1282995880@qq.com>
2019-09-26 18:53:46 +0800

    TextView

--- app/src/main/java/tqktqk/cn/android_learn/MainActivity.java
+++ app/src/main/java/tqktqk/cn/android_learn/MainActivity.java
@@ -1,13 +1,28 @@
  package tqktqk.cn.android_learn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnTextView = findViewById(R.id.btn_textview);
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到TextView演示界面
+                Intent intent = new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent );
+            }
+        });
     }
 }
---
+++ app/src/main/java/tqktqk/cn/android_learn/TextViewActivity.java
@@ -0,0 +1,37 @@
+package tqktqk.cn.android_learn;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class TextViewActivity extends AppCompatActivity {
+
+    private TextView mTv4;
+
+    private TextView mTv5;
+
+    private TextView mTv6;
+
+    private TextView mTv7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view);
+        mTv4 = findViewById(R.id.tv_4);
+        mTv4.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);//中划线
+        mTv4.getPaint().setAntiAlias(true);//去除锯齿
+
+        mTv5 = findViewById(R.id.tv_5);
+        mTv5.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);//下划线
+        mTv5.getPaint().setAntiAlias(true);//去除锯齿
+
+        mTv6 = findViewById(R.id.tv_6);
+        mTv6.setText(Html.fromHtml("<u>土豆在天上飞</u>"));
+
+        mTv7 = findViewById(R.id.tv_7);
+        mTv7.setSelected(true);
+    }
+}
